e190953c0a8a062129e5cda3dbf64a2140ab9ce4
Lee Jongyoung <leejongyoung@icloud.com>
2022-06-05 15:55:14 +0900

    Add project files

---
+++ main5.py
@@ -0,0 +1,24 @@
+import turtle as t
+
+t.bgcolor("black")
+t.color("pink")
+t.speed(0)
+
+for i in range(200):
+    t.pensize(i/50)
+    t.forward(i)
+    t.left(65)
+
+t.color("lightblue")
+t.setheading(270)
+
+for i in range(50):
+    t.pensize(25 - i/2)
+    t.forward(i/4)
+
+t.color("yellowgreen")
+t.setheading(60)
+for x in range(100):
+    t.pensize(100 - x)
+    t.forward(x/10)
+t.hideturtle()
